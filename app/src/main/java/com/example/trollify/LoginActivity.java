74f3613382d3ed5d19836c4ad545f3176b64830b
Waqqas Akhter <waqqas.akhter7@gmail.com>
2020-07-06 17:23:20 +0530

    Waqqas Commit Video#11, minor bugs

--- app/src/main/java/com/example/trollify/LoginActivity.java
+++ app/src/main/java/com/example/trollify/LoginActivity.java
@@ -1,29 +1,46 @@
 package com.example.trollify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class  LoginActivity extends AppCompatActivity {
 
     private Button LoginButton;
     private EditText UserEmail, UserPassword;
     private TextView NeedNewAccountLink;
 
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog loadingBar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         NeedNewAccountLink = (TextView) findViewById(R.id.register_account_link);
-        UserEmail =(EditText) findViewById(R.id.login_email);
+        UserEmail = (EditText) findViewById(R.id.login_email);
         UserPassword = (EditText) findViewById(R.id.login_password);
         LoginButton = (Button) findViewById(R.id.login_button);
+        loadingBar = new ProgressDialog(this);
 
         NeedNewAccountLink.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,6 +48,65 @@ public class LoginActivity extends AppCompatActivity {
                 SendUserToRegisterActivity();
             }
         });
+
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                AllowingUserToLogin();
+            }
+        });
+    }
+
+    private void AllowingUserToLogin()
+    {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this, "Please write your email...", Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            loadingBar.setTitle("Login");
+            loadingBar.setMessage("Please wait, while we are logging in to your Account...");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task)
+                        {
+                            if(task.isSuccessful())
+                            {
+                                SendUserToMainActivity();
+
+                                Toast.makeText(LoginActivity.this, "You are logged in successfully...", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().toString();
+                                Toast.makeText(LoginActivity.this, "Error occurred: "+message, Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+        }
+    }
+
+    private void SendUserToMainActivity()
+    {
+        Intent mainIntent = new Intent(LoginActivity.this, MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 
     private void SendUserToRegisterActivity() {
